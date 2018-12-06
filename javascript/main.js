f4179d14fc041e44ff2c29cc4a1e6c3a583657e8
Mike Miller <himikemiller@gmail.com>
2018-12-06 13:43:13 -0500

    fix 4 adds axios omg

--- javascript/main.js
+++ javascript/main.js
@@ -1,28 +1,19 @@
 function getLocation() {
   if (navigator.geolocation) {
     resetView();
-    navigator.geolocation.getCurrentPosition(showPosition);
+    window.gameData = {};
+    navigator.geolocation.getCurrentPosition(setPosition);
   } else {
     alert("Can't Geolocate. Try again.")
+    window.location.reload();
   }
 }
 
-function showPosition(position) {
-  lat = position.coords.latitude.toFixed(5);
-  lon = position.coords.longitude.toFixed(5);
-  encryptToURL(lat, lon);
-}
-
-function encryptToURL(lat, lon) {
-  fadeLoad();
-  var cryptLat = CryptoJS.AES.encrypt(lat.toString(), "Polo");
-  var cryptLon = CryptoJS.AES.encrypt(lon.toString(), "Polo");
-  var urlToSend = window.location.hostname + "/findme.html?lat=" + cryptLat + "&lon=" + cryptLon;
-  var smsText = "sms:?&body=" + urlToSend;
-
-  var button = document.getElementById('game');
-  button.href = smsText;
-  fadeButtonIn();
+function setPosition(position) {
+  var lat = position.coords.latitude.toFixed(5);
+  var lon = position.coords.longitude.toFixed(5);
+  window.gameData['position'] = [lat, lon];
+  encryptURL();
 }
 
 function fadeLoad() {
@@ -44,10 +35,35 @@ function resetView() {
   btn.classList.add('fade-in');
 }
 
-function fadeButtonIn() {
-  var el = document.querySelector('#game');
-  if (el.classList.contains('is-paused')) {
-    el.classList.remove('is-paused');
-    el.style.display = 'inline';
+function fadeButtonIn(bitlyURL) {
+  var button = document.querySelector('#game');
+  button.href = "sms:?&body=" + bitlyURL;
+  if (button.classList.contains('is-paused')) {
+    fadeLoad();
+    button.classList.remove('is-paused');
+    button.style.display = 'inline';
   }
+}
+
+function encryptURL() {
+  var lat = window.gameData.position[0];
+  var lon = window.gameData.position[1];
+  var cryptLat = CryptoJS.AES.encrypt(lat.toString(), "Polo");
+  var cryptLon = CryptoJS.AES.encrypt(lon.toString(), "Polo");
+  var urlToSend = 'https://' + 'cryptopolo.netlify.com' + "/findme.html?lat=" + cryptLat + "&lon=" + cryptLon;
+  setBitLink(urlToSend);
+}
+
+function setBitLink(encryptedURL) {
+  var endPoint = 'https://api-ssl.bitly.com/v3/shorten?login=o_1u0qnc7uj9&apiKey=R_c23b2db000fa489eb9daa3a856badf56&longUrl='
+  var encodedURL = encodeURIComponent(encryptedURL);
+
+  var url = endPoint + encodedURL;
+
+  axios.get(url)
+    .then(function (response) {
+      let bitlyData = response.data.data;
+      console.log(bitlyData);
+      fadeButtonIn(bitlyData.url);
+    });
 }
